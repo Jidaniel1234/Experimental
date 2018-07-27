75e8b5007308410314c1ee3a8b0974434c5a1bbf
Daniel Ji <Jidaniel1234@gmail.com>
2018-07-26 22:05:32 -0700

    First commit!
    Added what you first see, first part of landing page and nav - still need to add menu options

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(window).on("load", function() {
+
+  var winh = $(window).height();
+  var vidpos = $('.video').offset().top - $(window).scrollTop();
+  
+  $('.menu-open').on("click", function() {
+    $('.navcover').addClass('open');
+    $('.front').addClass('darken');
+    $('.nav').find('img').toggle();
+  });
+
+  $('.menu-close').on("click", function() {
+    $('.navcover').removeClass('open');
+    $('.front').removeClass('darken');
+    $('.nav').find('img').toggle();
+  });
+
+  $(window).on('scroll', function() {
+    if (winh * 2 / 5 < vidpos ) {
+      $('.video').play();
+    } else if (winh < vidpos) {
+      $('.video').pause();
+    } else {
+      $('.video').pause();
+    }
+  });
+
+  var controller = new ScrollMagic.Controller();
+  new ScrollMagic.Scene({
+    triggerElement: "#parallax",
+    triggerHook: "onEnter",
+  })
+  .duration('300%')
+  .setTween("#parallax", {
+    backgroundPosition: "50% 100%",
+    ease: Linear.None
+  })
+  .addIndicators()
+  .addTo(controller);
+
+});

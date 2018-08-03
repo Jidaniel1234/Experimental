95fd5922bebcba861c199ad30acd29b0e0bebe02
Daniel Ji <Jidaniel1234@gmail.com>
2018-08-03 15:43:38 -0700

    Commmiiiiitttss 8/3
    Bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 $(window).on("load", function() {
 
-  var winh = $(window).height();
-  var vidpos = $('.video').offset().top - $(window).scrollTop();
-  
+  //var winh = $(window).height();
+  //var vidpos = $('.video').offset().top - $(window).scrollTop();
+
   $('.menu-open').on("click", function() {
+    $('.navlist li a').addClass('oopen');
     $('.navcover').addClass('open');
     $('.front').addClass('darken');
     $('.nav').find('img').toggle();
   });
 
   $('.menu-close').on("click", function() {
+    $('.navlist li a').removeClass('oopen');
     $('.navcover').removeClass('open');
     $('.front').removeClass('darken');
     $('.nav').find('img').toggle();
   });
 
-  $(window).on('scroll', function() {
-    if (winh * 2 / 5 < vidpos ) {
-      $('.video').play();
-    } else if (winh < vidpos) {
-      $('.video').pause();
-    } else {
-      $('.video').pause();
-    }
-  });
+  //$(window).on('scroll', function() {
+  //  if (winh * 2 / 5 < vidpos ) {
+  //    $('.video').play();
+  //  } else if (winh < vidpos) {
+  //    $('.video').pause();
+  //  } else {
+  //    $('.video').pause();
+  //  }
+  //});
 
   var controller = new ScrollMagic.Controller();
   new ScrollMagic.Scene({

33b1f2b61e25169554b982e66eece129138dee4a
Daniel Ji <Jidaniel1234@gmail.com>
2018-08-28 21:22:44 -0700

    bigboicommit 8/28
    basically past week, worked on the preloading page, nav bars, animations for newer pages

--- js/home.js
+++ js/home.js
@@ -1,6 +1,10 @@
 $(window).on("load", function() {
-
-  var marker;
+  
+  var prevpanel;
+  var name;
+  var j;
+  var panels = ['front', 'viz', 'A', 'B', 'C', 'D'];
+  var marker = 0;
   const height = $(window).height();
   const scrollTop = $(window).scrollTop();
 
@@ -13,64 +17,34 @@ $(window).on("load", function() {
     origin: 'left',
   });
 
-  ScrollReveal().reveal('video', {
-    delay: 2000,
+  ScrollReveal().reveal('.front', {
+    delay: 400
+  });
+  ScrollReveal().reveal('.notimed', {
+    delay: 1000
   });
 
-  $('body').on('DOMMouseScroll mousewheel', function (e) {
-    if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
-      //scroll down
-      console.log('Down');
-      $('.front').removeClass('animated slideInDown');
-      $('.front').addClass('animated slideOutUp');
+  $('.front').on('DOMMouseScroll mousewheel', function (e) {
+    if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+      console.log("scrolling down");
+      $(this).removeClass('animated slideInDown');
+      $(this).addClass('animated slideOutUp');
     } else {
-      //scroll up
-      console.log('Up');
+      console.log("scrolling up");
+      if (this.className == 'panel viz') {
+        $('.front').removeClass('animated slideOutUp');
+        $('.front').addClass('animated slideInDown');
+      }
+    }
+  });
+
+  $('.viz').on('DOMMouseScroll mousewheel', function (e) {
+    if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) {
+      console.log("scrolling up");
       $('.front').removeClass('animated slideOutUp');
       $('.front').addClass('animated slideInDown');
     }
-    //prevent page fom scrolling
-    return false;
   });
-
-/*
-  var controller = new ScrollMagic.Controller();
-  new ScrollMagic.Scene({
-    triggerElement: "#parallax",
-    triggerHook: "onEnter",
-  })
-  .duration('200%')
-  .setTween("#parallax", {
-    backgroundPosition: "50% 100%",
-    ease: 'ease-in-out',
-  })
-  .addTo(controller);
-
-  var slides = $('.panel');
-
-  for (var i = 0; i < slides.length; i++) {
-    new ScrollMagic.Scene({
-      triggerElement: slides[i],
-      triggerHook: 'onLeave'
-    })
-    .setPin(slides[i])
-    .addTo(controller);
-  }
-
-  for later
-  var wipeAnimation = new TimelineMax()
-    .fromTo("section.panel.turgoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})
-    .fromTo("section.panel.green", 1, {x: "100%"}, {x: "0%", ease: Linear.easeNone})
-    .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone});
-
-  new ScrollMagic.Scene({
-    triggerElement: "#pinContainer",
-    triggerHook: "onLeave",
-    duration: '300%'
-  })
-  .setPin("#pinContainer")
-  .setTween(wipeAnimation)
-  .addIndicators()
-  .addTo(controller);
-*/
 });
+
+  //slidedown nav close and open stuff
--- js/main.js
+++ js/main.js
@@ -1,42 +1,107 @@
-$(window).on("load", function() {
-
-  hide();
-  //nav open close stuff
-  $('.menu-open').on("click", function() {
-    addNav();
-  });
-  $('.menu-close').on("click", function() {
-    removeNav();
-  });
-
+var loading_screen = pleaseWait({
+  logo: "css/images/wolf-logo-min.png",
+  backgroundColor: 'lightgrey',
+  loadingHtml: "<div class='spinner'><div class='double-bounce1'></div><div class='double-bounce2'></div></div>"
 });
 
-var hide = function() {
-  $('.subdiv1 .sub').hide();
-  $('.subdiv1 .title').hide();
-  $('.subdiv2 .title').hide();
-  $('.subdiv3 .title').hide();
-  $('.subdiv1 .sub').removeClass('animated fadeInDown');
-  $('.subdiv1 .title').removeClass('animated fadeInLeft');
-  $('.subdiv2 .title').removeClass('animated fadeInRight');
-  $('.subdiv3 .title').removeClass('aniamted fadeInLeft');
+$(window).on("load", function() {
+
+  loading_screen.finish();
+  $('.back').hide();
+  $('.forward').hide();
+  $('.up').hide();
+  hide();
+  //nav open close stuff
+  mopen();
+  mclose();
+  //navs for each slide
+    $('.f1, .title-1').click(function() {
+      $('.viz').removeClass('animated slideInRight');
+      $('.viz').addClass('animated slideOutRight');
+      $('.menu-open').hide();
+      $('.back').show();
+    });
+
+    $('.back').click(function() {
+      $('.viz').removeClass('animated slideOutRight');
+      $('.viz').addClass('animated slideInRight');
+      $('.back').hide();
+      $('.menu-open').show();
+      clear();
+    });
+
+    $('.s1, .title-2').click(function() {
+      $('.menu-open').hide();
+      $('.A').hide();
+      $('.viz').removeClass('animated slideInLeft');
+      $('.viz').addClass('animated slideOutLeft');
+      $('.forward').show();
+    });
+
+    $('.forward').click(function() {
+      $('.viz').removeClass('animated slideOutLeft');
+      $('.viz').addClass('animated slideInLeft');
+      $('.forward').hide();
+      $('.menu-open').show();
+      setTimeout(() => {$('.A').show();}, 1200);
+      clear();
+    });
+
+    $('.t1, .title-3').click(() => {
+      $('.menu-open').hide();
+      $('.A').hide();
+      $('.B').hide();
+      $('.viz').removeClass('animated slideInDown');
+      $('.viz').addClass('animated slideOutUp');
+      $('.up').show();
+    });
+
+    $('.up').click(() => {
+      $('.viz').removeClass('animated slideOutUp');
+      $('.viz').addClass('animated slideInDown');
+      $('.up').hide();
+      $('.menu-open').show();
+      setTimeout(() => {$('.A, .B').show();}, 1200);
+      clear();
+    });
+});
+function mopen() {
+    $('.menu-open').on("click", function() {
+      addNav();
+    });
+}
+function mclose() {
+    $('.menu-close').on("click", function() {
+      removeNav();
+    });
+}
+function hide() {
+  $('.ncleft').removeClass('animated fadeInLeft');
+  $('.ncright').removeClass('animated fadeInRight');
 }
 
-var removeNav = function() {
+function clear() {
+  setTimeout(() => {
+    $('.viz').removeClass('animated slideInDown slideInRight slideInLeft slideOutUp slideOutLeft slideOutRight');
+  }, 1200);
+}
+function removeNav() {
   $('.navcover').removeClass('open');
   $('.front').removeClass('darken');
   $('.nav').find('img').toggle();
   hide();
+  $('.back, .forward, .up').hide();
   $('html, body').css({
       overflow: 'auto',
       height: 'auto'
   });
 }
 
-var addNav = function() {
+function addNav() {
   $('.navcover').addClass('open');
   $('.front').addClass('darken');
   $('.nav').find('img').toggle();
+  $('.back, .forward, .up').hide();
   addSeq();
   $('html, body').css({
       overflow: 'hidden',
@@ -44,13 +109,7 @@ var addNav = function() {
   });
 }
 
-var addSeq = function() {
-  $('.subdiv1 .sub').show();
-  $('.subdiv1 .title').show();
-  $('.subdiv2 .title').show();
-  $('.subdiv3 .title').show();
-  $('.subdiv1 .sub').addClass('animated fadeInDown');
-  $('.subdiv1 .title').addClass('animated fadeInLeft');
-  $('.subdiv2 .title').addClass('animated fadeInRight');
-  $('.subdiv3 .title').addClass('animated fadeInRight');
+function addSeq() {
+  $('.ncleft').addClass('animated fadeInLeft');
+  $('.ncright').addClass('animated fadeInRight');
 }

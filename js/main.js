3c4ab58b2bfbf46834e5d15ccca88670a811e59e
Daniel Ji <Jidaniel1234@gmail.com>
2018-09-13 20:52:19 -0700

    holyitworks
    made the nav work!

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var loading_screen = pleaseWait({
   loadingHtml: "<div class='spinner'><div class='double-bounce1'></div><div class='double-bounce2'></div></div>"
 });
 var curslide;
+var through3choice;
 
 $(window).on("load", function() {
 
@@ -16,7 +17,39 @@ $(window).on("load", function() {
   mopen();
   mclose();
   //navs for each slide
-    $('.f1, .title-1').click(function() {
+    $('.navcover h1, .navcover p').click(function() {
+      removeNav();
+      console.log(this.className);
+      $('.front').removeClass('animated slideInDown');
+      $('.front').addClass('animated slideOutUp');
+      if (this.className == "title-1") {
+        curslide = 2;
+        $('.A').show();
+        $('.B').hide();
+        $('.C').hide();
+        $('.viz').removeClass('animated slideInRight');
+        $('.viz').addClass('animated slideOutRight');
+        $('.back').show();
+      } else if (this.className == "title-2") {
+        curslide = 3;
+        $('.A').hide();
+        $('.B').show();
+        $('.C').hide();
+        $('.viz').removeClass('animated slideInLeft');
+        $('.viz').addClass('animated slideOutLeft');
+        $('.forward').show();
+      } else if (this.className == "title-3") {
+        curslide = 4;
+        $('.A').hide();
+        $('.B').hide();
+        $('.C').show();
+        $('.viz').removeClass('animated slideInDown');
+        $('.viz').addClass('animated slideOutUp');
+        $('.up').show();
+      }
+    });
+
+    $('.f1').click(function() {
       $('.viz').removeClass('animated slideInRight');
       $('.viz').addClass('animated slideOutRight');
       $('.back').show();
@@ -31,24 +64,30 @@ $(window).on("load", function() {
       curslide = 1;
     });
 
-    $('.s1, .title-2').click(function() {
+    $('.s1').click(function() {
       $('.A').hide();
       $('.viz').removeClass('animated slideInLeft');
       $('.viz').addClass('animated slideOutLeft');
       $('.forward').show();
       curslide = 3;
+      through3choice = true;
     });
 
     $('.forward').click(function() {
+      if (through3choice) {
+        clear();
+      } else {
+        clearfast();
+      }
       $('.viz').removeClass('animated slideOutLeft');
       $('.viz').addClass('animated slideInLeft');
       $('.forward').hide();
       setTimeout(() => {$('.A').show();}, 1200);
-      clear();
       curslide = 1;
+      through3choice = false;
     });
 
-    $('.t1, .title-3').click(() => {
+    $('.t1').click(() => {
       $('.A').hide();
       $('.B').hide();
       $('.viz').removeClass('animated slideInDown');
@@ -96,6 +135,10 @@ function clear() {
     $('.viz').removeClass('animated slideInDown slideInRight slideInLeft slideOutUp slideOutLeft slideOutRight');
   }, 1200);
 }
+
+function clearfast() {
+  $('.viz').removeClass('animated slideInDown slideInRight slideInLeft slideOutUp slideOutLeft slideOutRight');
+}
 function removeNav() {
   $('.navcover').removeClass('open');
   $('.front').removeClass('darken');
@@ -106,14 +149,16 @@ function removeNav() {
     $('.back, .forward, .up').hide();
   } else if (curslide === 2) {
     $('.forward, .up').hide();
+    $('.back').show();
   } else if (curslide === 3) {
     $('.back, .up').hide();
+    $('.forward').show();
   } else if (curslide === 4) {
     $('.back, .forward').hide();
+    $('.up').show();
   } else {
     $('.back, .forward, .up').hide();
   }
-  $('').hide();
   $('html, body').css({
       overflow: 'auto',
       height: 'auto'
---
+++ js/maintest.js
@@ -0,0 +1,233 @@
+// During the page load, we use this filler
+var loading_screen = pleaseWait({
+  logo: "css/images/wolf-logo-min.png",
+  backgroundColor: 'lightgrey',
+  loadingHtml: "<div class='spinner'><div class='double-bounce1'></div><div class='double-bounce2'></div></div>"
+});
+var currslide = 0;
+
+$(window).on("load", function() {
+
+  // removes loading screen
+  loading_screen.finish();
+  hideArrows();
+
+  $(window).click(() => {
+    console.log($('.viz').attr('class'));
+  });
+});
+
+// f(x) for simplfication
+
+//defaults/justincase
+function clear() {
+  setTimeout(() => {
+    $('.viz').removeClass('animated slideInDown slideInRight slideInLeft slideOutUp slideOutLeft slideOutRight');
+  }, 1200);
+}
+function autoScrollFromLanding() {
+  $('.front').removeClass('animated slideInDown');
+  $('.front').addClass('animated slideOutUp');
+}
+// for nav and arrows
+function hideArrows() {
+  $('.back').hide();
+  $('.forward').hide();
+  $('.up').hide();
+}
+function addNav() {
+  $('.navcover').addClass('open');
+  $('.front').addClass('darken');
+  $('.menu-open').hide();
+  $('.menu-close').show();
+  $('.back, .forward, .up').hide();
+  $('html, body').css({
+      overflow: 'hidden',
+      height: '100%'
+  });
+}
+function removeNav() {
+  $('.navcover').removeClass('open');
+  $('.front').removeClass('darken');
+  $('.menu-close').hide();
+  $('.menu-open').show();
+  $('.ncleft').removeClass('animated fadeInLeft');
+  $('.ncright').removeClass('animated fadeInRight');
+  $('html, body').css({
+      overflow: 'auto',
+      height: 'auto'
+  });
+  switch (currslide) {
+    case 1:
+      $('.back').show();
+      $('.forward').hide();
+      $('.up').hide();
+      break;
+    case 2:
+      $('.forward').show();
+      $('.back').hide();
+      $('.up').hide();
+      break;
+    case 3:
+      $('.up').show();
+      $('.forward').hide();
+      $('.back').hide();
+      break;
+    default:
+      break;
+  }
+}
+
+// for OurTeam
+function showOurTeam() {
+  $('.A').show();
+  $('.B').hide();
+  $('.C').hide();
+  $('.viz').removeClass('animated slideInRight');
+  $('.viz').addClass('animated slideOutRight');
+  $('.back').show();
+  currslide = 1;
+}
+function hideOurTeam() {
+  clear();
+  setTimeout(() => {$('.A').hide(); clear();}, 1200);
+  $('.B').hide();
+  $('.C').hide();
+  $('.viz').removeClass('animated slideOutRight');
+  $('.viz').addClass('animated slideInRight');
+  $('.back').hide();
+  currslide = 0;
+}
+function clearA() {
+    $('.viz').removeClass('slideInRight slideOutRight');
+}
+
+// for OurRobot
+function showOurRobot() {
+  $('.B').show();
+  $('.A').hide();
+  $('.C').hide();
+  $('.viz').removeClass('animated slideInLeft');
+  $('.viz').addClass('animated slideOutLeft');
+  $('.forward').show();
+  currslide = 2;
+}
+function hideOurRobot() {
+  clear();
+  setTimeout(() => {$('.B').hide(); clear();}, 1200);
+  $('.A').hide();
+  $('.C').hide();
+  $('.viz').removeClass('animated slideOutLeft');
+  $('.viz').addClass('animated slideInLeft');
+  $('.forward').hide();
+  currslide = 0;
+}
+function clearB() {
+  $('.viz').removeClass('slideInLeft slideOutLeft');
+}
+
+// for Awards
+function showAwards() {
+  $('.C').show();
+  $('.A').hide();
+  $('.B').hide();
+  $('.viz').removeClass('animated slideInDown');
+  $('.viz').addClass('animated slideOutUp');
+  $('.up').show();
+  currslide = 3;
+}
+function hideAwards() {
+  clear();
+  setTimeout(() => {$('.C').hide(); clear();}, 1200);
+  $('.A').hide();
+  $('.B').hide();
+  $('.viz').removeClass('animated slideOutUp');
+  $('.viz').addClass('animated slideInDown');
+  $('.up').hide();
+  currslide = 0;
+}
+function clearC() {
+  $('.viz').removeClass('slideInDown slideOutUp');
+}
+
+// event handlers
+
+//nav event handlers
+$('.menu-open').click(() => {
+  addNav();
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      removeNav();
+    }
+  });
+});
+$('.menu-close').click(() => {
+  removeNav();
+  $(document).keyup(function(e) {
+    if (e.keyCode === 77 || e.keyCode === 78 || e.keyCode === 79) {
+      addNav();
+    }
+  });
+});
+
+//our team event handlers
+$('.title-1').click(() => {
+  clearB();
+  clearC();
+  currslide = 1;
+  autoScrollFromLanding();
+  removeNav();
+  showOurTeam();
+});
+$('.f1').click(() => {
+  showOurTeam();
+});
+$('.back').click(() => {
+  hideOurTeam();
+});
+
+//our robot event handlers
+$('.title-2').click(() => {
+  clearA();
+  clearC();
+  currslide = 2;
+  autoScrollFromLanding();
+  removeNav();
+  showOurRobot();
+});
+$('.s1').click(() => {
+  showOurRobot();
+});
+$('.forward').click(() => {
+  hideOurRobot();
+});
+
+//our awards event handlers
+$('.title-3').click(() => {
+  clearA();
+  clearB();
+  currslide = 3;
+  autoScrollFromLanding();
+  removeNav();
+  showAwards();
+});
+$('.t1').click(() => {
+  showAwards();
+});
+$('.up').click(() => {
+  hideAwards();
+});
+
+var map = {66: false, 67: false, 84: false};
+$(document).keydown(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = true;
+        if (map[66] && map[67] && map[84]) {
+            alert("I wish you the best endeavors in your future!");
+        }
+    }
+}).keyup(function(e) {
+    if (e.keyCode in map) {
+        map[e.keyCode] = false;
+    }
+});
